9f17b1c4c183417eba2dedbe900c8f705d40bc85
Collin Schwantes <collin@viralemergence.org>
2025-01-29 20:24:27 -0500

    adding data fields

--- inst/json_validation.R
+++ inst/json_validation.R
@@ -15,4 +15,12 @@ out_full
 out_min <- validator(json = "inst/minimal_example.json",verbose = TRUE)
 out_min
 
+# df <- data.frame("number" = 1:3,"sampleID" = letters[1:3],"missing" = c(TRUE,NA,FALSE))
+#
+# write.csv(df,file = "inst/simple.csv",row.names = FALSE)
+#
+# simple_df <- read.csv("inst/simple.csv")
+#
+# jsonlite::toJSON(simple_df,dataframe = "columns")
+
 
