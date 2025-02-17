95e41a0b9f09c39c0f1110d396145aa15205367d
Collin Schwantes <collin@viralemergence.org>
2025-02-17 16:46:31 -0500

    updating vignettes and examples with latest terms

--- R/wdds_schema.R
+++ R/wdds_schema.R
@@ -4,3 +4,17 @@
 #' See data standard JSON file for field descriptions.
 #'
 "wdds_schema"
+
+#' Wildlife Disease Data Standard
+#'
+#'
+#' See data standard JSON file for field descriptions.
+#'
+"disease_data_schema"
+
+#' Wildlife Disease Data Standard
+#'
+#'
+#' See data standard JSON file for field descriptions.
+#'
+"project_metadata_schema"
--- data-raw/data_required_fields.R
+++ data-raw/data_required_fields.R
@@ -1,9 +1,8 @@
 ## code to prepare `data_required_fields` dataset goes here
 
-wdds_schema <- jsonlite::read_json(path = here::here("inst/extdata/wdds_schema/wdds_schema.json"))
-required_data_fields <- wdds_schema$properties$data$required
+data_schema <- jsonlite::read_json(path = here::here("inst/extdata/wdds_schema/schemas/disease_data.json"))
+required_data_fields <- data_schema$required
 
 data_required_fields<- unlist(required_data_fields)
-
 
 usethis::use_data(data_required_fields, overwrite = TRUE)
