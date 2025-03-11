865056d99fdcee40201b1de37e27a2dabc025f34
Collin Schwantes <collin@viralemergence.org>
2025-03-11 16:30:33 -0400

    fixed typo

--- R/minimal_project_metadata.R
+++ R/minimal_project_metadata.R
@@ -1,7 +1,7 @@
 #' An example of minimal project metadata
 #'
 #'
-#' This is a minimal project metadata example. Its a list with the minimal
+#' This is a minimal project metadata example. It is a list with the minimal
 #' items required for project metadata.
 #'
 "minimal_project_metadata"
