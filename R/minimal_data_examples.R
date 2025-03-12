65176a04cb5c68965975e7e0ba5de7daabe1903c
Collin Schwantes <collin@viralemergence.org>
2025-03-12 13:20:41 -0400

    update raw data outputs

--- R/minimal_data_examples.R
+++ R/minimal_data_examples.R
@@ -5,3 +5,11 @@
 #' items required for project metadata.
 #'
 "minimal_project_metadata"
+
+#' An example of minimal disease data
+#'
+#'
+#' This is a minimal disease data example. It is a data frame with the minimal
+#' items required for disease data.
+#'
+"minimal_disease_data"
