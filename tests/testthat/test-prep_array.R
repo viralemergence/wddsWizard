aa4b8b9c588c2ba62e5ec6697533dd8281c28c9e
collinschwantes <collin@viralemergence.org>
2025-09-29 11:53:34 -0400

    fixing issue with tests

--- tests/testthat/test-prep_array.R
+++ tests/testthat/test-prep_array.R
@@ -3,13 +3,11 @@ test_that("prep array works", {
   nested_list <- list(list("formats" = list("formats" = "csv",
   "formats" = "fasta")))
 
-
-
-  expect_no_failure(  prep_array(nested_list))
+  expect_no_failure(prep_array(nested_list))
 })
 
 
 test_that("prep array fails", {
   hello <- c("hello","world")
-  expect_failure(  prep_array(hello))
+  expect_error(  prep_array(hello))
 })
